e64d627d88ce4a6c5ecffbd55f464778a67a3835
aayushmishra321 <maayush081@gmail.com>
2026-04-06 13:00:00 +0530

    feat(backend): mount schedule routes into express application instance

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ const appointmentRoutes = require('./routes/appointmentRoutes');
 const billingRoutes = require('./routes/billingRoutes');
 const prescriptionRoutes = require('./routes/prescriptionRoutes');
 const recordRoutes = require('./routes/recordRoutes');
+const scheduleRoutes = require('./routes/scheduleRoutes');
+
 
 const app = express();
 
@@ -15,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/', apiLimiter);
 app.use('/api/records', recordRoutes);
+app.use('/api/schedules', scheduleRoutes);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/patients', patientRoutes);
